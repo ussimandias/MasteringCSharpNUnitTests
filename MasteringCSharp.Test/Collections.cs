b8ad3280312b56d03fd544bcb88d75e4ad358a83
Usman Tarfa <utarfa@fortegra.com>
2015-11-16 12:08:35 -0500

    Lists and Dictionaries in Generics

--- MasteringCSharp.Test/Collections.cs
+++ MasteringCSharp.Test/Collections.cs
@@ -44,6 +44,84 @@ namespace MasteringCSharp.Test
             List<string> names = new List<string>();
             names.Add("Fred");
             Assert.AreEqual("Fred", names[0]);
+
+            Assert.AreEqual(1, names.Count);
+            names.Add("Petr");
+            Assert.AreEqual(2, names.Count);
+
+            names.RemoveAt(0);
+            Assert.AreEqual(1, names.Count);
+            Assert.AreEqual("Petr", names[0]);
+
+            names[0] = "Paula";
+            Assert.AreEqual("Paula", names[0]);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            /*
+             * var integers = new  List<int>(50){10, 20};
+             * is
+             * Equivalent to:
+             */
+
+            var tmp = new List<int>(50);
+            tmp.Add(10);
+            tmp.Add(20);
+            var integers = tmp;
+
+            integers = new List<int>{integers.Count};
+            Assert.AreEqual(2, integers[0]);
+        }
+
+        [Test]
+        public void DictionaryBasics()
+        {
+            var map = new Dictionary<string, int>();
+            map.Add("MGS V", 1);
+            map["BattleFront"] = 2;
+
+            //foreach (var entry in map)
+            //{
+            //    Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
+            //}
+
+            /*
+             * Use this if your sure there is a value...
+             * int Value = map["MGS V"];
+             * 
+             */
+
+            int Value;
+            bool KeyFound = map.TryGetValue("BattleFront", out Value);
+            Assert.IsTrue(KeyFound);
+            Assert.AreEqual(2, Value);
+
+            /*
+             * OR
+             */
+
+            //int Value2;
+            //bool KeyFound1 = map.TryGetValue("foo", out Value2);
+            //Assert.IsFalse(KeyFound1);
+            //Assert.AreEqual(0, Value2);
+
+            //like a  List<> you can ask for 'count' in the dictionary.. 
+            Assert.AreEqual(2, map.Count);
+
+            //Object initializers
+            map = new Dictionary<string, int>
+            {
+                {"abc", 3},
+                {"def", 4}
+            };
+            int Value3;
+            bool KeyFound3 = map.TryGetValue("abc", out Value3);
+            Assert.IsTrue(KeyFound3);
+            Assert.AreEqual(3, Value3);
+
         }
 
         [Test]
