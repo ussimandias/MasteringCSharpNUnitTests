7d6e723f5887415a730577777978634e36d38a37
Usman Tarfa <utarfa@fortegra.com>
2015-11-18 15:02:57 -0500

    Delegates in CSharp

---
+++ MasteringCSharp.Test/DelegatesTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MasteringCSharp.Test
+{
+    [TestFixture]
+    public class DelegatesTest
+    {
+        [Test]
+        public void SingleMethodInterface()
+        {
+         IInt32Action action = new Delegates();   
+            action .DoIt(10);
+        }
+
+        [Test]
+        public void SingleDelegateMethodFromMethod()
+        {
+            Delegates target = new Delegates("John");
+            Int32Action action = new Int32Action(target.RandomRob);
+
+            action.Invoke(5);
+            action(6);
+        }
+
+        [Test]
+        public void DelegateFromSaticMethod()
+        {
+            Delegates target = new Delegates();
+            Int32Action action = new Int32Action(Delegates.StaticRob);
+
+            action.Invoke(7);
+        }
+
+
+        [Test]
+        public void MultiCast()
+        {           
+            Int32Action action1 = new Int32Action(Delegates.StaticRob);
+            Int32Action action2 = new Int32Action(Delegates.StaticRob2);
+
+            Int32Action action3 = action1 + action2;
+            action3(20);
+        }
+    }
+}
---
+++ MasteringCSharp/Delegates.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasteringCSharp
+{
+    public delegate void Int32Action(int value);
+
+    public interface IInt32Action
+    {
+        void DoIt(int value);
+    }
+
+    public class Delegates : IInt32Action
+    {
+        string name;
+
+        public Delegates(string name)
+        {
+            this.name = name;
+        }
+
+        public Delegates() : this("unknown")
+        {
+            
+        }
+
+        public void DoIt(int value)
+        {
+            Console.WriteLine("Interface Implementation: {0}", value);
+        }
+
+       public void  RandomRob(int value)
+        {      
+            Console.WriteLine("{0}: Delegate Implementation: {1}", name, value);     
+        }
+
+       public static void StaticRob(int value)
+       {
+           Console.WriteLine("Static Method 1: {0}", value);
+       }
+
+       public static void StaticRob2(int value)
+       {
+           Console.WriteLine("Static Method 2: {0}", value);
+       }
+    }  
+}
